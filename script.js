ec72d4fb0e3ccb6819d04587eea6669896fc3a8d
Rithika0912 <rithikanavanee@gmail.com>
2025-10-30 12:17:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Initialize Typed.js
+var typed = new Typed("#typed", {
+  strings: [
+    "a Passionate Developer 💻",
+    "a Cloud Enthusiast ☁️",
+    "a Creative Thinker 🌟",
+    "an Innovator 🚀"
+  ],
+  typeSpeed: 80,
+  backSpeed: 40,
+  loop: true
+});
+
+
+// Initialize AOS (Animate On Scroll)
+AOS.init({
+  duration: 1000,
+  once: true,
+});
